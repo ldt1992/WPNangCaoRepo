c699c8504dc03683eef802e2f4c3a19233ba0960
ldt1992 <ldt1992@gmail.com>
2017-09-28 11:06:54 +0700

    tự tạo plugin đầu tiên cho mình

---
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QA11
+ * Date: 9/28/2017
+ * Time: 10:53 AM
+ */
+
+/**
+ * Plugin Name: My First Plugin Demo // Tên của plugin
+ * Plugin URI: http://hocwp.net // Địa chỉ trang chủ của plugin
+ * Description: Đây là plugin đầu tiên mà tôi viết dành riêng cho WordPress, chỉ để học tập mà thôi. // Phần mô tả cho plugin
+ * Version: 1.0 // Đây là phiên bản đầu tiên của plugin
+ * Author: Duc Tai // Tên tác giả, người thực hiện plugin này
+ * Author URI: http://ductai.com // Địa chỉ trang chủ của tác giả
+ * License: GPLv2 or later // Thông tin license của plugin, nếu không quan tâm thì bạn cứ để GPLv2 vào đây
+ */
